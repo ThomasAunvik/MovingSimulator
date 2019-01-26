3567489b8fdc5b86c1c98b53ecc3a79c0763f4fb
Thomas Johansen <tmjoh1998@hotmail.com>
2019-01-26 19:19:23 +0100

    "Upploaded script and files for audio prefab and fmod"

---
+++ Assets/Scripts/SadAudio.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class SadAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string eventPath = "";
+
+    private EventInstance instance;
+    private ParameterInstance sad;
+    private ParameterInstance observing;
+
+    [Range(0,1)]
+    public float sadLevel;
+    [Range(0, 1)]
+    public float observingLevel;
+
+    private float lerpSad = 0f;
+    private float lerpObs = 0f;
+
+    public void Start() {
+        instance = RuntimeManager.CreateInstance(eventPath);
+        instance.getParameterByIndex(0, out sad);
+        instance.getParameterByIndex(1, out observing);
+        instance.start();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        lerpSad = Mathf.Lerp(lerpSad, sadLevel, 0.05f);
+        lerpObs = Mathf.Lerp(lerpObs, observingLevel, 0.05f);
+
+
+        sad.setValue(lerpSad);
+        observing.setValue(lerpObs);
+    }
+}
