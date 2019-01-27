367d47cf43cd28670f07250008a6c33d7b55af9b
tobbun <tobbun@gmail.com>
2019-01-27 13:40:45 +0100

    Started on setting up an Audiosettings object
    Start Menu should now work. Quit button does not have anything hooked up but otherwise it cute and nice

---
+++ Assets/Scripts/UI/AudioSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    public float volume{get;set;}
+
+    public Camera MainCamera;
+
+    //public AudioListener audioListener;
+
+    void Update(){
+
+        CheckForMainCamera();
+        
+        AdjustVolume();
+
+    }
+
+    void CheckForMainCamera(){
+        if(MainCamera == null){
+            MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            //audioListener = MainCamera.GetComponent<audioListener>();
+        }        
+    }
+
+    void AdjustVolume(){
+        
+    }
+
+
+}
---
+++ Assets/Scripts/UI/s_StartMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class s_StartMenuScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("I love you all so much.");
+    }
+
+    // Update is called once per frame
+    public void StartGame(){
+        SceneManager.LoadScene("StarterScene");
+    }
+}
