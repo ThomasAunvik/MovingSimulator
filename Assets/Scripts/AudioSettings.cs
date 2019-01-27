3352e4d66f906e872d229ec88ae2e21529448b03
Thomas Johansen <tmjoh1998@hotmail.com>
2019-01-27 13:52:03 +0100

    audio settings now exist

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+    
+public class AudioSettings : MonoBehaviour
+{
+
+    string masterBusString = "Bus:/";
+    Bus masterBus;
+
+    [Range(0, 2f)]
+    public float volume;
+
+    void Start()
+    {
+        masterBus = RuntimeManager.GetBus(masterBusString);
+        masterBus.getVolume(out volume, out volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        masterBus.setVolume(volume);
+    }
+}
